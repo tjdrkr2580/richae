1f9def9eb548fd6e260a5ecfd10da92dbe95e084
tjdrkr2580 <tjdrkr2580@naver.com>
2023-01-22 06:42:57 +0900

    추가 | favicon, desc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from "@components/Footer";
+import Header from "@components/Header";
 import Home from "@pages/Home";
 import GlobalStyled from "@utils/GlobalStyled";
 import React from "react";
@@ -6,11 +8,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <GlobalStyled />
       <BrowserRouter>
+        <GlobalStyled />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
