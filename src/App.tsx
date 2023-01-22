238d2b7edbb219ea11ba39c3dd14dd7a1d885b08
tjdrkr2580 <tjdrkr2580@naver.com>
2023-01-23 00:09:59 +0900

    ADD | install modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import Footer from "@components/Footer";
 import Header from "@components/Header";
 import Home from "@pages/Home";
 import GlobalStyled from "@utils/GlobalStyled";
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 function App() {
   return (
     <>
